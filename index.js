f690e64505fcb23c079786cc040f048250b99d62
Robert Carroll <robertcarrollsales@gmail.com>
2022-01-21 14:42:29 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const name = ['Guadalupe', 'Ollie', 'Aki'];
+const event = 'surprise';
+
+function writeCards(name, event) {
+    let newArray = [];
+    for (let i = 0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+
+}
+
+
+
+
+
+
+
